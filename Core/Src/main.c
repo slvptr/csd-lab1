b871481f1a5a0fb5188210fb7a96df1b12facbf5
slvptr <callmepedro@mail.ru>
2023-10-06 18:29:34 +0300

    do refactor

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -57,6 +57,8 @@ void SystemClock_Config(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 extern void initialise_monitor_handles(void);
+void flash_yellow();
+void flash_red();
 
 /* USER CODE END 0 */
 
@@ -65,94 +67,103 @@ extern void initialise_monitor_handles(void);
  * @retval int
  */
 int main(void) {
-  /* USER CODE BEGIN 1 */
-  initialise_monitor_handles();
+    /* USER CODE BEGIN 1 */
+    initialise_monitor_handles();
 
-  /* USER CODE END 1 */
+    /* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+    /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
 
-  /* USER CODE BEGIN Init */
+    /* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+    /* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* Configure the system clock */
+    SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+    /* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+    /* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
-  /* USER CODE BEGIN 2 */
-  _Bool is_long_press = 0;
-  int last_pressed = 0;
-  int wait = 500;
-  int cnt = 0;
-  uint32_t input_timeout = 5000;
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1) {
-	int was_pressed = 0;
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    /* USER CODE BEGIN 2 */
+    /* USER CODE BEGIN 2 */
     _Bool states[10000];
-    int button_state = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15);
-    is_long_press = 0;
-    if (!button_state) {
-      last_pressed = HAL_GetTick();
-    }
-    while (!button_state) {
-      was_pressed = 1;
-      if (HAL_GetTick() - last_pressed >= wait) {
-        is_long_press = 1;
-      }
-      button_state = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15);
-    }
 
-    if (was_pressed) {
-      if (!is_long_press) {
-    	printf("short pressed\n");
-        states[cnt] = false;
-        HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
-        HAL_Delay(250);
-        HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
-      } else {
-      	printf("long pressed\n");
-        states[cnt] = true;
-        HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
-        HAL_Delay(250);
-        HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
-      }
-      cnt++;
-    }
-    if (cnt != 0 && (HAL_GetTick() - last_pressed >= input_timeout)) {
-      for (int i = 0; i < cnt; i++) {
-        if (states[i]) {
-          HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
-          HAL_Delay(1000);
-        } else {
-          HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
-          HAL_Delay(250);
+    uint32_t last_pressed_tick = 0;
+    uint32_t long_press_time = 500;
+    uint32_t input_timeout = 5000;
+
+    uint32_t number_of_presses = 0;
+
+    /* USER CODE END 2 */
+
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    while (1) {
+        _Bool was_pressed = false;
+        _Bool last_pressed_long = false;
+
+        GPIO_PinState button_state = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15);
+
+        if (button_state == GPIO_PIN_RESET) {
+            last_pressed_tick = HAL_GetTick();
         }
-        HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
-        HAL_Delay(250);
-      }
-      cnt = 0;
+        while (button_state == GPIO_PIN_RESET) {
+            was_pressed = true;
+            if (HAL_GetTick() - last_pressed_tick >= long_press_time) {
+                last_pressed_long = true;
+            }
+            button_state = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15);
+        }
+
+        if (was_pressed) {
+            if (last_pressed_long) {
+                printf("[pressed] long\n");
+                states[number_of_presses] = true;
+                flash_red();
+            } else {
+                printf("[pressed] short\n");
+                states[number_of_presses] = false;
+                flash_yellow();
+            }
+            number_of_presses++;
+        }
+        if (number_of_presses != 0 && (HAL_GetTick() - last_pressed_tick >= input_timeout)) {
+            for (int i = 0; i < number_of_presses; i++) {
+                if (states[i]) {
+                    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+                    HAL_Delay(1000);
+                } else {
+                    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+                    HAL_Delay(250);
+                }
+                HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+                HAL_Delay(250);
+            }
+            number_of_presses = 0;
+        }
     }
-  }
 }
 
 /* USER CODE END WHILE */
 
 /* USER CODE BEGIN 3 */
+void flash_yellow() {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+    HAL_Delay(250);
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+}
 
+void flash_red() {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
+    HAL_Delay(250);
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+}
 
 /* USER CODE END 3 */
 
@@ -161,51 +172,51 @@ int main(void) {
  * @retval None
  */
 void SystemClock_Config(void) {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {
-    0
-  };
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {
-    0
-  };
+    RCC_OscInitTypeDef RCC_OscInitStruct = {
+        0
+    };
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {
+        0
+    };
 
-  /** Configure the main internal regulator output voltage
-   */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    /** Configure the main internal regulator output voltage
+     */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-   * in the RCC_OscInitTypeDef structure.
-   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 15;
-  RCC_OscInitStruct.PLL.PLLN = 216;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig( & RCC_OscInitStruct) != HAL_OK) {
-    Error_Handler();
-  }
+    /** Initializes the RCC Oscillators according to the specified parameters
+     * in the RCC_OscInitTypeDef structure.
+     */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 15;
+    RCC_OscInitStruct.PLL.PLLN = 216;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 4;
+    if (HAL_RCC_OscConfig( & RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
 
-  /** Activate the Over-Drive mode
-   */
-  if (HAL_PWREx_EnableOverDrive() != HAL_OK) {
-    Error_Handler();
-  }
+    /** Activate the Over-Drive mode
+     */
+    if (HAL_PWREx_EnableOverDrive() != HAL_OK) {
+        Error_Handler();
+    }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-   */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
-    RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+        RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig( & RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
-    Error_Handler();
-  }
+    if (HAL_RCC_ClockConfig( & RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
+        Error_Handler();
+    }
 }
 
 /* USER CODE BEGIN 4 */
@@ -217,11 +228,11 @@ void SystemClock_Config(void) {
  * @retval None
  */
 void Error_Handler(void) {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1) {}
-  /* USER CODE END Error_Handler_Debug */
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+    __disable_irq();
+    while (1) {}
+    /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef USE_FULL_ASSERT
@@ -233,9 +244,9 @@ void Error_Handler(void) {
  * @retval None
  */
 void assert_failed(uint8_t * file, uint32_t line) {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+    /* USER CODE BEGIN 6 */
+    /* User can add his own implementation to report the file name and line number,
+       ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+    /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
